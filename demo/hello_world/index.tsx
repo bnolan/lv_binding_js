949d6484edb6e74e12ed6bf1525181755377afd9
Ben Nolan <bnolan@gmail.com>
2025-02-13 21:27:39 +1300

    Render some mastodon posts

--- demo/hello_world/index.tsx
+++ demo/hello_world/index.tsx
@@ -1,12 +1,43 @@
-import { Button, Render, Text, View } from "lvgljs-ui";
-import React from "react";
+import { Button, Line, Render, Text, View } from "lvgljs-ui";
+import React, { useState } from "react";
+
+const URL = "https://mastodon.social/api/v1/timelines/public";
+
+const dark = "#303030";
+const light = "#f3f3f3";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) =>
+      setData(data.map((post) => post.content.replace(/<[^>]*>?/gm, ""))),
+    )
+    .catch((error) => console.error("Error:", error));
+
   return (
-    <View style={{ "background-color": "blue" }}>
-      <Button style={{ "background-color": "black" }}>
-        <Text style={{ "text-color": "white" }}>Hello?!</Text>
-      </Button>
+    <View
+      style={{
+        width: 320,
+        height: 240,
+        "background-color": light,
+        display: "flex",
+        "flex-direction": "column",
+      }}
+    >
+      {data.map((post) => (
+        <>
+          <Text style={{ "text-color": dark }}>{post.slice(0, 32)}...</Text>
+          <Line
+            style={{ "line-color": dark }}
+            points={[
+              [0, 0],
+              [320, 0],
+            ]}
+          />
+        </>
+      ))}
     </View>
   );
 }
